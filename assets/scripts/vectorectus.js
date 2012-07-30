2576c0d63e3083a249175b74f4019fe6469b0ff1
Evan Krambuhl <thekrambuhl@gmail.com>
2012-07-30 16:32:50 -0700

    Vectorectus Library update

--- assets/scripts/vectorectus.js
+++ assets/scripts/vectorectus.js
@@ -1,37 +1,40 @@
-$(window).ready(function() { Vectorectus.Vectorectus() });
+$(window).ready(function() { 
+    $('[data-vectorectus]').vectorectus({
+        
+    });
+});
 
-var vectorectusVectors = {
-
-};
-
-
-
-var Vectorectus = {    
-    Vectorectus : function () {
-        var SelectorToData = function (selector) {
-            names = selector.split('.');
-                        
-            return vectorectusVectors [names[0]] [names[1]];
-        };
-    
-        $(".vectorectus").each(function() {
-            if ($(this).attr('id') === undefined) 
-                $(this).attr('id', 'vector-'  + $(this).data('shape') + "-" + (Math.floor(Math.random()*90000) + 10000));
-                
-            var id      = $(this).attr('id'),
-                shape   = $(this).data('shape');
-                
-            var data    = SelectorToData(shape),
-                width   = data[0],
-                height  = data[1],
-                path    = data[2];
-                
-            var color   = $(this).css('color');
-               // options = $(this).data('shape-options');
-                
-            var vector = Raphael(id, "100%", "100%");
-            vector.path(path).attr({'stroke-width': '0','stroke-opacity': '0','fill': color });
-            vector.setViewBox(0, 0, width, height);
-        });
-    }
-};
+(function ($) {
+    $.fn.extend({
+        vectorectus : function (options) {
+            var SelectorToData = function (selector) {
+                names = selector.split('.') || ['', ''];          
+                return shapes [names[0]] [names[1]];
+            };           
+            
+            
+            var defaults = { };             
+            var options = $.extend(defaults, options),
+                index   = 0;
+            
+            return this.each(function() {
+                if ($(this).attr('id') === undefined) 
+                    $(this).attr('id', 'vectorectus-'  + $(this).data('vectorectus') + "-" + index++;
+                    
+                var id      = $(this).attr('id'),
+                    shape   = $(this).data('vectorectus'),
+                    
+                    data    = SelectorToData(shape),
+                    path    = data[2],
+                    box     = path.getBBox(),
+                    
+                    color   = $(this).css('color');
+                   // options = $(this).data('shape-options');
+                    
+                var vector = Raphael (id, "100%", "100%");
+                vector.path(path).attr ({'stroke-width': '0','stroke-opacity': '0','fill': color });
+                vector.setViewBox (box.x, box.y, box.width, box.height);
+            });
+        }
+    });
+})(jQuery);
